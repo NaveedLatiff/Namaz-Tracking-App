500a8b5fd41bebf60435180a378b9d6457f97ded
Naveed latif <naveedlatif526@gmail.com>
2025-08-16 08:17:09 -0700

    All Done.

--- firebase.js
+++ firebase.js
@@ -24,15 +24,7 @@ import {
     Timestamp
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB9_nqZXGwTCZ1edUMF3kHxFb5Tisakovo",
-    authDomain: "practice-d29e9.firebaseapp.com",
-    projectId: "practice-d29e9",
-    storageBucket: "practice-d29e9.firebasestorage.app",
-    messagingSenderId: "1086461013407",
-    appId: "1:1086461013407:web:5f1009519e907d009aa1aa",
-    measurementId: "G-T5FBEH9HZY"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
